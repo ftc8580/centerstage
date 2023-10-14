134d9ad9c925c7866608262d0e3e9371ca59f16b
Bill Broughton <wlbrough@gmail.com>
2023-10-14 12:46:21 -0400

    Stub suspend subsystem

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystem/SuspendSubsystem.kt
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.subsystem
+
+import com.arcrobotics.ftclib.command.SubsystemBase
+import org.firstinspires.ftc.teamcode.hardware.HardwareManager
+
+class SuspendSubsystem(hardware: HardwareManager) : SubsystemBase() {
+    init {
+        // TODO: Initialize hardware components from HardwareManager
+    }
+
+    fun raiseRobot() {
+        TODO()
+    }
+}
